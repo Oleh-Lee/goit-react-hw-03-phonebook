e6018923a920003089df6d457131188692900c15
Oleh-Lee <rrrgo3@gmail.com>
2020-03-13 00:13:17 +0200

    Updates

---
+++ precache-manifest.c9ce52eaea7737c1e1f56956ad784491.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdaf966d2bd58726ede5d2b59dfdbe49",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "11ad1d4d464670e3644a",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.a31f8f04.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.a31f8f04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d2ea8a5d79bcf404401",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.db98c723.chunk.js"
+  },
+  {
+    "revision": "6d3bf054ab910ade8107",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.968b66e7.js"
+  }
+]);
